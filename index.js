4bda334d44df188f557c53abe5b1f26b67a5cfa3
Prithvi Reddy <prithvi2k2@gmail.com>
2021-09-12 21:46:53 +0530

    Web Server ALPHA

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Following the best practices are necessary for proper maintainance of code
-// For info refer: 
+// For info refer:
 // 1.https://discordjs.guide/creating-your-bot/command-handling.html
 // 2.https://discordjs.guide/creating-your-bot/event-handling.html
 
 const fs = require('fs');
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+// EXPRESS.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
 // Creates a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -23,3 +28,12 @@ for (const file of eventFiles) {
 }
 // Login to Discord with your client's token
 client.login(token);
+
+// Simple Express web server
+app.get('/', (req, res) => {
+	res.send('Bot is LIVE!');
+});
+
+app.listen(port, () => {
+	console.log('Bot listening at default route!');
+});
