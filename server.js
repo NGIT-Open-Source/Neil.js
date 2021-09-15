e7f858782f33a5e9affca86042483696b9e784e4
Prithvi Reddy <prithvi2k2@gmail.com>
2021-09-15 06:37:31 +0530

    Configured with Heroku
    Heroku dynamically listens to it's very own dynamic ports(probably :80), configured accordingly

--- server.js
+++ server.js
@@ -4,14 +4,14 @@
 
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send('BOT is LIVE!');
 });
 
 function startServer() {
-	app.listen(3000, () => {
+	app.listen(port, () => {
 		console.log('Server is LIVE');
 	});
 }
