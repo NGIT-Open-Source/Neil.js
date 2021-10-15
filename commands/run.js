dbc069e385c44aa47ab4d7d9fb7a9406e9795953
Prithvi Reddy <prithvi2k2@gmail.com>
2021-10-15 22:12:19 +0530

    /run Bug fix + Improvements
    - Can queue requests, previously crashed on simultaneous requests by single or multiple users
    - If a user triggers multiple /run instances, only the latest request will be persisted and all previous PENDING requests by that user will be destroyed
    - Fixed compiler/interpreter conflicts

--- commands/run.js
+++ commands/run.js
@@ -21,44 +21,12 @@ const langs = {
 	'TypeScript':'typescript-3.9.5',
 };
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('run')
-		.setDescription('👩‍💻Runs your <code/>;')
-		.addStringOption(option => {
-			option
-				.setName('language')
-				.setDescription('Select programming language')
-				.setRequired(true);
-			for (const [lang, val] of Object.entries(langs)) {
-				option.addChoice(`${lang}`, `${val}`);
-			}
-			return option;
-		}),
-	async execute(interaction) {
-		const lang = interaction.options.getString('language');
-		await interaction.reply({ content: 'Send your code in this channel enclosed in a code block. Request expires in 15 minutes!', ephemeral: true });
-		// Check if the message is a codeblock or not
-		const filter = msg => msg.content.startsWith('```') && msg.content.endsWith('```');
-		// Create collector to collect multi-line input via codeblock
-		// Waits for codeblock reply. Ends collector after 15 minutes if no response is received
-		const collector = interaction.channel.createMessageCollector({ max: 1, filter, time: 900000 });
-
-		collector.on('collect', async message => {
-			const code = format(message.content);
-			const output = await run(code, lang);
-			await interaction.followUp({
-				content: `[Source code](${message.url}) by ${interaction.user}`,
-				embeds : [embed(output)],
-				components: [link(output.url)] });
-		});
-
-		collector.on('end', async (collected) => {
-			console.log(`Collected ${collected.size} items`);
-			if (collected.size == 0) await interaction.followUp({ content: 'Token expired', ephemreal: true });
-		});
-	},
-};
+// Version of selected language
+// This object is honestly a bad addition, but djs has left no other way to access OptionChoice `name` directly
+const versions = {};
+Object.keys(langs).forEach(key => {
+	versions[langs[key]] = key;
+});
 
 // Parses code from code block
 const format = code => {
@@ -66,11 +34,12 @@ const format = code => {
 	const end = code.lastIndexOf('\n');
 	return code.slice(start, end);
 };
+
 // POST code to wandbox API
-const run = async (code, lang) => {
+const run = async (code, version) => {
 	const form = {
 		'code':code,
-		'compiler': lang,
+		'compiler': version,
 		'save':true,
 	};
 	const headers = {
@@ -84,11 +53,17 @@ const run = async (code, lang) => {
 };
 
 // EMBED CONSTRUCTORS
-const embed = output => new MessageEmbed()
-	.setColor('#15e854')
-	.setTitle('OUTPUT')
-	.setDescription(`\`\`\`\n${output.program_message}\`\`\``)
-	.setFooter(`Exit Status Code: ${output.status}`);
+const embed = (output, lang) => {
+	let out = output.program_message;
+	if (out == undefined) out = output.compiler_message;
+	return new MessageEmbed()
+		.setColor('#15e854')
+		.setTitle('output')
+		.setDescription(`\`\`\`\n${out}\`\`\``)
+		.addFields(
+			{ name: 'language', value: `${lang}` })
+		.setFooter(`Exit Status Code: ${output.status}`);
+};
 
 // BUTTON CONSTRUCTORS
 const link = url => new MessageActionRow()
@@ -98,3 +73,67 @@ const link = url => new MessageActionRow()
 			.setURL(url)
 			.setStyle('LINK'),
 	);
+
+// Active Collectors array, stores userIds with active collectors, follow further code to understand
+const active = new Map();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('run')
+		.setDescription('👩‍💻Runs your <code/>;')
+		.addStringOption(option => {
+			option
+				.setName('language')
+				.setDescription('Select programming language')
+				.setRequired(true);
+			for (const [lang, version] of Object.entries(langs)) {
+				option.addChoice(`${lang}`, `${version}`);
+			}
+			return option;
+		}),
+	async execute(interaction) {
+
+		// options.getString('language') will actually return option choice `value`, and not choice `name`
+		// https://discordjs.guide/interactions/replying-to-slash-commands.html#parsing-options
+		const version = interaction.options.getString('language');
+		const lang = versions[version];
+		const user = interaction.user.id;
+
+		// prompt user to send code
+		await interaction.reply({ content: `Send your **${lang}** code enclosed in a code block. Request expires in 15 minutes!`, ephemeral: true });
+
+		// Check if any received message by user is a codeblock, if not wait for reply until timer ends
+		// `filter` returns a boolean, code will be processed only when it returns True
+		function filter(message) {
+			return (message.author.id == user) &&
+				message.content.startsWith('```') && message.content.endsWith('```');
+		}
+
+		// Collector is created when /run command is triggered
+
+		// Check if user has already created a collector
+		// if yes, destroy previous collector
+		const activeCollector = active.get(user);
+		if (activeCollector) activeCollector.stop();
+
+		// Create new collector to collect multi-line input via codeblock
+		// Waits for codeblock reply. Ends collector after 15 minutes if no response is received
+		const collector = interaction.channel.createMessageCollector({ max: 1, filter, time: 900000 });
+		active.set(user, collector);
+
+		collector.on('collect', async message => {
+			const code = format(message.content);
+			const output = await run(code, version);
+			await message.reply({
+				embeds : [embed(output, lang)],
+				components: [link(output.url)] });
+		});
+
+		collector.on('end', async (collected) => {
+			active.delete(user);
+			const tag = interaction.user.tag;
+			if (collected.size == 0) console.log(`${tag}'s /run request expired!`);
+			else console.log(`${tag}'s /run request processed!`);
+		});
+	},
+};
